b1f195e681abaa8a31a4e33c993de2bee7eca6f1
huangmickey <916m.huang@gmail.com>
2021-11-09 11:47:49 -0800

    Guided project complete

--- src/processing/sketches/Main.java
+++ src/processing/sketches/Main.java
@@ -13,6 +13,18 @@ public class Main extends PApplet {
     final int LIGHTBLUE = color(180, 225, 255);
     final int WHITE = color(255, 255, 255);
 
+    String[] dayNames = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+    int[] temperature = {59, 70, 64, 84, 65, 70, 70};
+    int[] chanceOfRain = {90, 50, 0, 0, 60, 30, 30};
+    Weather[] weather = { Weather.Rain, Weather.PartlyCloudy, Weather.Cloudy, Weather.Sunny, Weather.Cloudy, Weather.PartlyCloudy, Weather.PartlyCloudy };
+
+    enum Weather {
+        Sunny,
+        PartlyCloudy,
+        Cloudy,
+        Rain
+    }
+
     PImage CloudyIcon;
     PImage PartlyCloudyIcon;
     PImage SunnyIcon;
@@ -38,29 +50,57 @@ public class Main extends PApplet {
 
         drawGraphLines();
 
-
-        // for loop goes here
-            // Weather
-            // GraphNode
-            // DayOfTheWeek
+        for (int i = 0; i < dayNames.length; i++) {
+            drawGraphNode(i);
+            drawWeather(i);
+            drawDayOfWeek(i);
+        }
 
         drawAverageTemperature();
     }
 
     private void drawGraphNode(int i) {
+        int x = width/14 + i * width/7;
+        int y = 450 - temperature[i] * 5;
 
+        fill(LIGHTBLUE);
+        if (i < 6) {
+            int nextY = 450 - temperature[i  + 1] * 5;
+            int nextX = width/14 + (i + 1) * width/7;
+            line(x, y, nextX, nextY);
+        }
+        ellipse(x, y, 15, 15);
     }
 
     private void drawWeather(int i) {
+        fill(BLUE);
+        rect(width/7 * i, height/2, width/7, 150);
+        image(getImage(weather[i]), width/7 * (i + 0.5f), height/2 + 40, 80, 80);
 
+        fill(LIGHTBLUE);
+        textSize(32);
+        text(temperature[i] + "°", width/7 * i + width/14, height/2 + 105);
+        fill(WHITE);
+        textSize(16);
+        text(chanceOfRain[i] + "%", width/7 * i + width/14, height/2 + 135);
     }
 
     private void drawDayOfWeek(int i) {
-
+        fill(LIGHTBLUE);
+        textSize(18);
+        text(dayNames[i], width/7 * i + width/14, height - 15);
     }
 
     private void drawAverageTemperature() {
+        float temp = 0;
+        for (int i = 0; i < temperature.length; i++) {
+            temp += temperature[i];
+        }
+        temp = temp / temperature.length;
 
+        fill(WHITE);
+        textSize(18);
+        text("Average Temperature: " + (int)temp + "°", width - 150, 170);
     }
 
     private void drawGraphLines() {
